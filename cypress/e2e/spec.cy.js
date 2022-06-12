3fca5986fe812bad8245a5b495ffb23ec0ea7b61
mattiacoll <64754556+mattiacoll@users.noreply.github.com>
2022-06-12 14:33:20 +0200

    Added first tests

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,37 @@
+describe('Default with text element', () => {
+  it('Server started', () => {
+    cy.visit('http://localhost:8080/tests/text.html');
+  });
+
+  it('Has #marquee element and is of width 500px', () => {
+    cy
+      .get('#marquee')
+      .invoke('width')
+      .should('be.eq', 500);
+  });
+
+  it('#marquee should be outside of view after initializaton', () => {
+    cy
+      .window()
+      .invoke('start')
+      .get('.js-marquee-wrapper')
+      .then($el => $el[0].getBoundingClientRect().x)
+      .should('be.eq', 500);
+  });
+
+  it('#marquee should be outside of view after duration', () => {
+    cy
+      .wait(9860) // 8960 + 1000 - 10
+      .get('.js-marquee-wrapper')
+      .then($el => $el[0].getBoundingClientRect().x)
+      .should('be.lt', -380);
+  });
+
+  it('#marquee should restart', () => {
+    cy
+      .wait(200) // 8960 + 1000 - 10
+      .get('.js-marquee-wrapper')
+      .then($el => $el[0].getBoundingClientRect().x)
+      .should('be.lt', 480);
+  });
+});
